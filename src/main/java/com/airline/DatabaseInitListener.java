1db82910145afeffedcc8f4eeb99aa0f1d28ee5b
Ranithaa28 <ranithaaravichandran@gmail.com>
2026-07-22 07:08:57 +0530

    Clean up unused imports in DatabaseInitListener

--- src/main/java/com/airline/DatabaseInitListener.java
+++ src/main/java/com/airline/DatabaseInitListener.java
@@ -2,22 +2,14 @@ package com.airline;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 
 public class DatabaseInitListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        System.out.println("Database auto-initialization is disabled. Using manual Workbench tables.");
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        // Cleanup if necessary
     }
 }
